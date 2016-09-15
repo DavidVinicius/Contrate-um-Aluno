2cc0b2f2b2292cf8b5dfb074e05ec0499df4385e
Matheus Picioli <matheus.picioli98@gmail.com>
2016-09-15 18:44:04 -0300

    vou remover a pasta .idea

---
+++ Controller/Vagas.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: INFOLABS
+ * Date: 15/09/2016
+ * Time: 18:33
+ */
---
+++ Model/DataBase.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: INFOLABS
+ * Date: 15/09/2016
+ * Time: 18:37
+ */
+    class DataBase
+    {
+
+    }
+
+?>

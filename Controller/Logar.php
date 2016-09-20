75529c7243945e3da30f552e282ee29f45a11522
Matheus Picioli <matheus@picioli>
2016-09-19 21:44:26 -0300

    Atualizado para com objetos

--- Controller/Logar.php
+++ Controller/Logar.php
@@ -1,13 +1,28 @@
 <?php
-    include "../Model/DataBase.class.php";
-    include "CreateVarSessions.class.php";
+    include_once "../Model/DataBase.class.php";
+    include_once "CreateVarSessions.class.php";
+    include_once "VerificaSeEstaLogado.class.php";
     $RealizaLogin = new DataBase();
     session_start();
-    
 
+    $VerificaLogado = new VerificaSeEstaLogado();
+
+/*  Se estiver logado, irá jogar as variaveis de sessão aqui, caso não, retorna false
+    e da um clean nas variáveis de sessão, redireciona pra página inicial
+
+*/
+    $VarSessions = $VerificaLogado->EstaLogado();
+
+    //Se estiver logado iremos pegar as variáveis de sessão assim:
+    $id     = $VarSessions->getSessionID(); //Pega o ID, que está em $_SESSION["id"];
+    $user   = $VarSessions->getSessionUsuario(); //Pega o user ...
+    $pass   = $VarSessions->getSessionSenha();
+    $nivel  = $VarSessions->getSessionNivel();
+
+/* VOU SUBSTITUIR ISSO AQUI EMBAIXO, COM A CLASSE QUE FIZ
     $usuario    = (isset($_POST['usuario'])) ? $_POST['usuario'] : null;
     $senha      = (isset($_POST['senha'])) ? $_POST['senha'] : null;
-
+*/
 
     $consulta = $RealizaLogin -> SearchQuery("usuario","WHERE email = '$usuario' AND senha = '$senha'");
 

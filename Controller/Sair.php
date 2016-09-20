860970e7977b325d8635d1531445a4a2c8ec11a3
Matheus Picioli <matheus@picioli>
2016-09-20 13:35:33 -0300

    Retirado o parâmetro passado pela URL(desnecessário)

--- Controller/Sair.php
+++ Controller/Sair.php
@@ -1,13 +1,10 @@
 <?php
-    $acao = $_GET['action'];
 
-    if($acao == "sair"){
-        session_start();
-        session_unset($_SESSION['usuario']);
-        session_unset($_SESSION['senha']);
-        session_destroy();
-
-        header("Location:./../Index.php");
-    }
+    session_start();
+    session_unset($_SESSION['usuario']);
+    session_unset($_SESSION['senha']);
+    session_destroy();
+    
+    header("Location:./../Index.php");
         
 ?>

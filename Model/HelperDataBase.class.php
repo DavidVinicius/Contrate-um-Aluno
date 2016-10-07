e03cdd34b4f505d34b0500767a23df058f4569fa
Matheus Picioli <matheus.picioli98@gmail.com>
2016-10-06 22:22:23 -0300

    Criado classe pai para CRUD, hur dur

---
+++ Model/HelperDataBase.class.php
@@ -0,0 +1,35 @@
+<?php
+    include "DataBase.class.php";
+
+    class HelperDataBase
+    {
+        public function InstanciaDB()
+        {
+            return $DB = new Database();
+        }
+
+        public function Create($Table, $Data)
+        {
+            $DB = $this->InstanciaDB();
+            return $Result = $DB->InsertQuery($Table, $Data);
+        }
+
+        public function Read($Table, $Condition)
+        {
+            $DB = $this->InstanciaDB();
+            return $Result = $DB->SearchQuery($Table, $Condition);
+        }
+
+        public function Update($Field, $NewValue, $Id)
+        {
+            $DB = $this->InstanciaDB();
+            return $Result = $DB->UpdateQuery("aluno", $Field, $NewValue, " WHERE idAluno = $Id");
+        }
+
+        public function Delete($Table, $Condition)
+        {
+            $DB = $this->InstanciaDB();
+            return $Result = $DB->DeleteQuery($Table, $Condition);
+        }
+    }
+?>

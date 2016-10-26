f13296daf954abc699fcbe3465dcca32b3c5de70
Matheus Picioli <matheus.picioli98@gmail.com>
2016-10-26 19:44:56 -0200

    Arrumando ver curriculo

--- View/Shared/Candidatos.php
+++ View/Shared/Candidatos.php
@@ -1,8 +1,10 @@
 <?php
     include_once "Model/DataBase.class.php";
-    $DB = new DataBase();
+    include_once "Model/ModelQualificacoes.class.php";
+    include_once "Model/ModelAluno.class.php";
+    $Aluno = new ModelAluno();
     $id = $_SESSION['id'];
-    $Consulta = $DB->SearchQuery("aluno");
+    $Consulta = $Aluno->ReadAluno();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,15 +23,20 @@
             <div class="col s12 m12">
                 <?php
                 while( $linha = mysqli_fetch_assoc($Consulta) ){
+                  $idAluno = $linha['idAluno'];
+                  $Qualificacao = new ModelQualificacoes();
+                  $ResultadoQ = $Qualificacao->ReadQualificacoes("where codAluno = $idAluno");
                 ?>
                 <div class="col s12 m6">
                     <div class="card horizontal hoverable">
-                        <div class="card-image activator"> 
-                            <img src="Images/Padrao/PerfilPadrao.png" alt=""> 
+                        <div class="card-image activator">
+                            <img src="Images/Padrao/PerfilPadrao.png" alt="">
                             <span class="card-title"><?=$linha['nome']?></span>
                         </div>
                         <div class="card-content">
-                            <p><?=$linha['qualificacoes']?></p>
+                            <?php while($qualificacao = mysqli_fetch_assoc($ResultadoQ)){ ?>
+                            <p><?=$qualificacao['competencia']?></p>
+                            <?php } ?>
                             <a href="OnePage.php?link=Candidato&id=<?=$linha['idAluno']?>"><button class="btn blue">Ver perfil</button></a>
                         </div>
                     </div>

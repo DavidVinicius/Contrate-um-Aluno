55d2d9fef73337674bd37b8aa6d4ab894455bc88
Matheus Picioli <matheus.picioli98@gmail.com>
2016-11-01 22:22:38 -0200

    Exibindo as fotos

--- View/Shared/Menu.php
+++ View/Shared/Menu.php
@@ -1,10 +1,13 @@
 
 <?php
-
-
  $nivel = $_SESSION['nivel'];
-
+ $idUsuario = $_SESSION['id'];
 if($nivel == 1){
+    include_once "Model/ModelAluno.class.php";
+    $aluno = new ModelAluno();
+    $fetch = mysqli_fetch_assoc($aluno->ReadAluno("where codUsuario = $idUsuario"));
+    $fotoAluno = $fetch['foto'];
+    var_dump($fotoAluno);
     echo "<div class='navbar-fixed'>
 
 
@@ -18,14 +21,14 @@ if($nivel == 1){
                 <li><a href='OnePage.php?link=Vagas'>Vagas</a></li>
                 <li><a href='OnePage.php?link=VerCurriculo'>Curriculo</a></li>
                 <li><a href='OnePage.php?link=Perfil'>Perfil</a></li>
-               <li><a href='' data-activates='Configuracoes' class='abrir'><img src='Images/Padrao/PerfilPadrao.png' class='circle responsive-img valign' width='40px' style='margin-top:10px'><span class='badge red circle white-text'>2</span></a></li>
+               <li><a href='' data-activates='Configuracoes' class='abrir'><img src='Images/Upload/".$fotoAluno."' class='circle responsive-img valign' width='40px' style='margin-top:10px'><span class='badge red circle white-text'>2</span></a></li>
                 <li><a href='./Controller/Sair.php'>Sair</a></li>
                 <li><span style='margin-right:5%'>&nbsp &nbsp</span></li>
               </ul>
               <ul class='side-nav' id='menuLateral'>
                <li><div class='userView'>
-                  <img class='background responsive-img' src='Images/Office.jpg'>
-                  <a href='#!user'><img class='circle' src='images/Padrao/PerfilPadrao.png'></a>
+                  <img class='background responsive-img' src='Images/Upload/Office.jpg'>
+                  <a href='#!user'><img class='circle' src='images/Upload/".$fotoAluno."'></a>
                   <a href='#!name'><span class='white-text name'>Aqui vai o Nome</span></a>
                   <a href='#!email'><span class='white-text email'>email@email.com</span></a>
                 </div></li>
@@ -38,7 +41,7 @@ if($nivel == 1){
               <ul class='side-nav' id='Configuracoes'>
                   <li><div class='userView'>
                   <img class='background responsive-img' src='Images/Office.jpg'>
-                  <a href='#!user'><img class='circle' src='images/Padrao/PerfilPadrao.png'></a>
+                  <a href='#!user'><img class='circle' src='Images/Upload/".$fotoAluno."'></a>
                   <a href='#!name'><span class='white-text name'>John Doe</span></a>
                   <a href='#!email'><span class='white-text email'>jdandturk@gmail.com</span></a>
                     </div></li>
@@ -52,6 +55,11 @@ if($nivel == 1){
       </div>";
 }
 else if ($nivel == 2){
+  include_once "Model/ModelEmpresa.class.php";
+  $empresa = new ModelEmpresa();
+  $fetch = mysqli_fetch_assoc($empresa->ReadEmpresa("where codUsuario = $idUsuario"));
+  $fotoEmpresa = $fetch['foto'];
+  var_dump($fotoEmpresa);
     echo "<div class='navbar-fixed'>
 
 
@@ -66,14 +74,14 @@ else if ($nivel == 2){
                 <li><a href='OnePage.php?link=VerEmpresa'>Suas Informações</a></li>
                 <li><a href='OnePage.php?link=CriarVaga'>Criar Vaga</a></li>
                 <li><a href='OnePage.php?link=Perfil'>Perfil</a></li>
-               <li><a href='' data-activates='Configuracoes' class='abrir'><img src='Images/Padrao/PerfilPadrao.png' class='circle responsive-img valign' width='40px' style='margin-top:10px'><span class='badge red circle white-text'>2</span></a></li>
+               <li><a href='' data-activates='Configuracoes' class='abrir'><img src='Images/Upload/".$fotoEmpresa."' class='circle responsive-img valign' width='40px' style='margin-top:10px'><span class='badge red circle white-text'>2</span></a></li>
                 <li><a href='./Controller/Sair.php'>Sair</a></li>
                 <li><span style='margin-right:5%'>&nbsp &nbsp</span></li>
               </ul>
               <ul class='side-nav' id='menuLateral'>
                <li><div class='userView'>
                   <img class='background responsive-img' src='Images/Office.jpg'>
-                  <a href='#!user'><img class='circle' src='images/Padrao/PerfilPadrao.png'></a>
+                  <a href='#!user'><img class='circle' src='Images/Upload/".$fotoEmpresa."'></a>
                   <a href='#!name'><span class='white-text name'>Aqui vai o Nome</span></a>
                   <a href='#!email'><span class='white-text email'>email@email.com</span></a>
                 </div></li>
@@ -87,7 +95,7 @@ else if ($nivel == 2){
               <ul class='side-nav' id='Configuracoes'>
                   <li><div class='userView'>
                   <img class='background responsive-img' src='Images/Office.jpg'>
-                  <a href='#!user'><img class='circle' src='images/Padrao/PerfilPadrao.png'></a>
+                  <a href='#!user'><img class='circle' src='Images/Upload/".$fotoEmpresa."'></a>
                   <a href='#!name'><span class='white-text name'>John Doe</span></a>
                   <a href='#!email'><span class='white-text email'>jdandturk@gmail.com</span></a>
                     </div></li>

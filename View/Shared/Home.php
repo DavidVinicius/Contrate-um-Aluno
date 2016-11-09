14df6b0a667a45904e47b7686aa9408b000aa813
David Vinicius <de.175@hotmail.com>
2016-11-09 19:42:00 -0200

    Alguns detalhes

--- View/Shared/Home.php
+++ View/Shared/Home.php
@@ -1,12 +1,68 @@
+<?php
+  include_once "Model/ModelEntrevistas.class.php";
+  include_once "Model/ModelAluno.class.php";
+  $Aluno            = new ModelAluno();
+  $Entrevista       = new ModelEntrevistas();
+  $idUsuario        = $_SESSION['id'];
+  $ConsultaAluno    = mysqli_fetch_object($Aluno->ReadAluno("where codUsuario = $idUsuario"));
+  $idAluno          = $ConsultaAluno->idAluno;
+  $ConsultaNum      = mysqli_num_rows($Entrevista->ReadEntrevista("where codAluno = $idAluno"));
+  $ConsultaEntrevista = $Entrevista->ReadEntrevista("where codAluno = $idAluno");
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Contrate um Aluno</title>
+    <script type="text/javascript" src="js/jquery-3.1.0.min.js"> </script>
+    <script type="text/javascript" src="js/materialize.min.js"> </script>
+    <script type="text/javascript" src="js/Home.js"> </script>
 </head>
 <body>
     <div class="container">
-        <h1 class="flow-text">Usuario: <?php echo $_SESSION["usuario"] ?></h1> 
+        <div class="row">
+          <div class="col s12 m12">
+            <h1 class="flow-text">Usuario: <?php echo $_SESSION["usuario"] ?></h1>
+
+            <?php
+              if ($ConsultaNum > 0) {
+                ?>
+                <div class="col s12 m12">
+                <ul class="collection">
+                <?php
+
+                      include_once "Model/ModelMensagens.class.php";
+                      include_once "Model/ModelEmpresa.class.php";
+                      while ($result = mysqli_fetch_object($ConsultaEntrevista)) {
+
+                  ?>
+                        <li class="collection-item avatar hoverable">
+                          <div class="col s4 m1">
+                            <img src="Images/Padrao/PerfilPadrao.png" alt="" class="circle" />
+                            <a href="#!" class="secondary-content btn-flat btn-large waves-effect waves-light"><i class="material-icons blue-text">add</i></a>  
+                          </div>
+                          <div class="col s8 m8 pull-m1">
+                            <span class="title">Empresa:</span>
+                            <br>
+                              <span class="flow-text">Você tem uma nova Entrevista </span>
+
+                          </div>
+
+                        </li><br>
+
+                  <?php
+                }
+                ?>
+                  </ul>
+                </div>
+                <?php
+              }
+              else{
+                echo "you don't have a new e-mail";
+              }
+             ?>
+          </div>
+        </div>
     </div>
 </body>
 </html>
--- View/Shared/Vaga.php
+++ View/Shared/Vaga.php
@@ -10,8 +10,7 @@
     $salario       = $Assoc['salario'];
     $cargaHoraria  = $Assoc['cargaHoraria'];
     $requisitos    = $Assoc['requisitos'];
-    $beneficios    = $Assoc['beneficios'];
-    $CodEmpresa    = $Assoc['codEmpresa'];
+      $CodEmpresa    = $Assoc['codEmpresa'];
     $Result2       = $DB->SearchQuery("empresa", "where idEmpresa = $CodEmpresa");
     $EmpresaAssoc   = mysqli_fetch_assoc($Result2);
     $nomeEmpresa   = $EmpresaAssoc['nome'];
@@ -20,43 +19,79 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Contrate um Aluno</title>
+    <script type="text/javascript" src="js/jquery-3.1.0.min.js"> </script>
+    <!-- <script type="text/javascript" src="js/materialize.min.js"> </script> -->
+    <script type="text/javascript" src="js/Vaga.js"></script>
+    <style media="screen">
+    .parallax-container {
+      height: 200px;
+    }
+    </style>
 
 </head>
 <body >
     <div class="container">
 
+
         <div class="row">
-            <h1 class='flow-text center-align'><?php echo $titulo. " - ". $nomeEmpresa;?></h1>
+            <h1 class='flow-text center-align'><?php echo $titulo;?></h1>
             <div class="col m12 s12 l12">
+              <div class="row">
 
-                    <div class="col m12 s12">
-                        <h5 class='flow-text'>Descrição:</h5>
-                        <?php echo $descricao;?>
-                    </div>
-                    <div class="col m12 s12">
-                        <h5 class='flow-text'>Requisitos:</h5>
-                        <?php echo $requisitos;?>
+                <div class="input-field col s12 m12">
+                  <label for="empresa">Empresa:</label>
+                  <input type="text" name="empresa" id="empresa" value="<?=$nomeEmpresa ?>" readonly class="flow-text" style="font-size:25px">
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12 m6">
+                  <label for="salário">Salário:</label>
+                  <input type="text" name="salario" id="salario" class="flow-text" value="R$ <?php echo number_format($salario,2,',','.'); ?>" style="font-size:25px">
+                </div>
+                <div class="input-field col s12 m6">
+                  <label for="cargaHoraria">Carga horária:</label>
+                  <input type="text" name="cargaHoraria" id="cargaHoraria" class="flow-text" value="<?= $cargaHoraria ?>" style="font-size:25px" readonly="true">
+                </div>
+              </div>
+              <div class="input-field col s12 m12">
+                <p class="flow-text">
+                  Benefícios:
 
-                    </div>
-                    <div class="col m12 s12">
-                        <h5 class='flow-text'>Beneficios:</h5>
-                        <?php echo $beneficios;?>
+                <?php
+                  require_once "Model/ModelBeneficiosVaga.class.php";
+                  $Beneficio = new ModelBeneficiosVaga();
+                  $Consulta  = $Beneficio->ReadBeneficiosVaga("where codVaga = $IdVaga");
+                    while ($ResultB = mysqli_fetch_object($Consulta)) {
+                      ?>
+                        <span class="chip"><?= $ResultB -> beneficio ?></span>
 
-                    </div>
+                      <?php
+                    }
+                 ?>
+               </p>
+              </div>
+              <div class="row">
+                <div class="input-field col s12 m12">
+                  <label for="requisitos">Requisitos:</label>
+                  <textarea name="requisitos" rows="8" cols="40" class="materialize-textarea" readonly="true"><?php echo $requisitos;?></textarea>
+                </div>
+                <div class="input-field col s12 m12">
+                    <label for="descricao">Descrição:</label>
+                    <textarea name="descricao" id="descricao" cols="30" rows="10" class="materialize-textarea" readonly="true"><?php echo $descricao;?></textarea>
+                </div>
+              </div>
+
+
+
             </div>
         </div>
+
         <div class="row">
-            <span class='flow-text'>Salário: <?php echo number_format($salario,2,',','.'); ?></span><br>
-            <span class='flow-text'>Carga horária: <?= $cargaHoraria ?></span>
-        </div>
-        <div class="row">
-            <a href="" class='btn blue'>Candidatar-se</a>
+            <a href="" class='btn btn-large blue'>Candidatar-se</a>
         </div>
     </div>
-    <script src='js/jquery-3.1.0.min.js'></script>
-    <script src="js/materialize.js"></script>
-    <script src="js/angular.min.js"></script>
+
 
 </body>
 </html>

e18eb4d9c9635976f11a1d64fed01ccfeabb1bd1
Matheus Picioli <matheus.picioli98@gmail.com>
2016-11-18 17:44:57 -0200

    esqueci de subir (note firma)

--- View/Shared/Candidatos.php
+++ View/Shared/Candidatos.php
@@ -1,8 +1,14 @@
 <?php
     include_once "Model/DataBase.class.php";
+    include_once "Model/ModelQualificacoes.class.php";
+    include_once "Model/ModelAluno.class.php";
     $DB = new DataBase();
     $id = $_SESSION['id'];
-    $Consulta = $DB->SearchQuery("aluno");
+
+
+    $ConsultaAluno  = $DB->SearchQuery("aluno");
+    $Qualificacoes  = new ModelQualificacoes();
+    $Aluno      = new ModelAluno();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,8 +26,11 @@
         <div class="row">
             <div class="col s12 m12">
                 <?php
-                while( $linha = mysqli_fetch_assoc($Consulta) ){
-                ?>
+                while( $linha = mysqli_fetch_assoc($ConsultaAluno) ){
+                    $idUsuario  = $_SESSION['id'];
+                    $LerAluno   = $Aluno->ReadAluno("where codUsuario = $idUsuario");
+                    $FetchAluno = mysqli_fetch_assoc($LerAluno);
+                 ?>
                 <div class="col s12 m6">
                     <div class="card horizontal hoverable">
                         <div class="card-image activator"> 
@@ -29,7 +38,9 @@
                             <span class="card-title"><?=$linha['nome']?></span>
                         </div>
                         <div class="card-content">
-                            <p><?=$linha['qualificacoes']?></p>
+                            <?php while($qualificacao = mysqli_fetch_assoc($Ler)){ ?>
+                            <span class="chip blue"><?= $qualificacao['competencia'] ?></span>
+                            <?php } ?>
                             <a href="OnePage.php?link=Candidato&id=<?=$linha['idAluno']?>"><button class="btn blue">Ver perfil</button></a>
                         </div>
                     </div>
